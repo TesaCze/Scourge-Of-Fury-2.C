d2d68d26605463cce7aafd11a4c9c35ad6ebc9f0
Tesa <tesacze@gmail.com>
2023-05-17 23:48:13 +0200

    fdfdsafasfd

--- main.js
+++ main.js
@@ -9,13 +9,11 @@ let game = new Game(ctx, canvas, camera);
 let sword = new Sword(-10, -75, 75, 75, 0, [["../animations/Sword/Idle/hit.png"],["../animations/Sword/Attack/hit.png","../animations/Sword/Attack/hit1.png","../animations/Sword/Attack/hit2.png"]],"sword");
 let map;         
 let newGameObject;
-
-
-
+let currentLvl;
 
 async function Start() 
 {
-    await LoadFromJson('enemaci_test.json')
+    await LoadFromJson("enemaci_test.json")
     setInterval(Update, 16);
     setInterval(AnimationUpdate, 120);
 }
@@ -54,4 +52,31 @@ function Update() {
 }
 function AnimationUpdate() {
     game.AnimationUpdate()
+}
+
+
+//----- MODAL ---------
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
